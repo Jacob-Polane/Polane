254ff9aa9ad424b4a8f9c13ef91c9151eb7bbeb6
Polane Mahloko <polane.mahloko@boxfusion.io>
2026-02-26 22:38:15 +0200

    push backend

---
+++ backend/src/Module/polane.Polane.Domain/Domain/Artists.cs
@@ -0,0 +1,10 @@
+﻿using Abp.Domain.Entities.Auditing;
+
+namespace polane.Polane.Domain.Domain
+{
+    public class Artists : FullAuditedEntity
+    {
+        public string Name { get; set; }
+        public long Genre { get;set; }
+    }
+}
---
+++ backend/src/Module/polane.Polane.Domain/Domain/Events.cs
@@ -0,0 +1,10 @@
+﻿using Abp.Domain.Entities.Auditing;
+
+namespace polane.Polane.Domain.Domain
+{
+    public class Events : FullAuditedEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
